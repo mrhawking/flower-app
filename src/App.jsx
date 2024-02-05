e379ada053d3bb0d1003eddae7a0a90911f12bd4
Nataly Tsvetkova <mrhoking666@gmail.com>
2024-02-05 15:30:52 +0300

    Favorite context was created

--- src/App.jsx
+++ src/App.jsx
@@ -1,179 +1,107 @@
-import { useState, useEffect } from "react";
 import Catalog from "./components/Catalog/Catalog";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Slogan from "./components/Slogan";
-import { updateFavoriteFlowersIds, fetchAvailableFlowers, fetchFavoriteFlowers } from "./http";
+import { FavoriteContextProvider } from "./store/FavoriteContext";
 
 
 function App() {
-  const [availableFlowers, setAvailableFlowers] = useState([]);
-  const [favoriteFlowersIds, setFavoriteFlowersIds] = useState([]);
-  const [isFetching, setIsFetching] = useState(false);
-  const [error, setError] = useState(null);
-
-
-  useEffect(() => {
-    async function getFlowers() {
-      setIsFetching(true);
-      try {
-        const [availableFlowers, favoriteFlowers] = await Promise.all([
-          fetchAvailableFlowers(),
-          fetchFavoriteFlowers()
-        ]);
-
-        setAvailableFlowers(availableFlowers);
-        setFavoriteFlowersIds(favoriteFlowers);
-      } catch (error) {
-        setError((error.message || 'Ошибка загрузки данных'));
-      }
-      setIsFetching(false);
-    }
-    getFlowers();
-  }, []);
-
-  // useEffect(() => {
-  //   async function getFlowers() {
-  //     try {
-  //       const availableFlowers = await fetchAvailableFlowers();
-  //       setAvailableFlowers(availableFlowers);
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   }
-
-  //   async function getFavoriteFlowers() {
-  //     try {
-  //       const favoriteFlowers = await fetchFavoriteFlowers();
-  //       setFavoriteFlowersIds(favoriteFlowers);
-  //     } catch (error) {
-  //       console.log(error)
-  //     }
-  //   }
-  //   getFlowers();
-  //   getFavoriteFlowers();
-  // }, []);
-
-
-  async function handleAddFavorite(flowerId) {
-    setFavoriteFlowersIds((prevFavoriteFlowersIds) => {
-      return [...prevFavoriteFlowersIds, flowerId]
-    });
-
-    try {
-      await updateFavoriteFlowersIds([...favoriteFlowersIds, flowerId])
-    } catch (error) {
-      setFavoriteFlowersIds(favoriteFlowersIds);
-      console.log(error)
-    }
-  }
-
-  async function handleRemoveFavorite(flowerId) {
-    setFavoriteFlowersIds((prevFavoriteFlowersIds) => {
-      return prevFavoriteFlowersIds.filter(id => id !== flowerId)
-    });
-
-    try {
-      await updateFavoriteFlowersIds(favoriteFlowersIds.filter(id => id !== flowerId));
-    } catch (error) {
-      setFavoriteFlowersIds(favoriteFlowersIds);
-      console.log(error)
-    }
-  }
-
-  function handleFavoriteStatus(flowerId, isFavorite) {
-    if (!isFavorite) {
-      handleAddFavorite(flowerId);
-    } else {
-      handleRemoveFavorite(flowerId);
-    }
-  } 
-
-
+ 
   return (
-    <>
+    <FavoriteContextProvider>
       <Header />
       <main>
         <Slogan />
-        {error && <p>Ошибка</p>}
-        {!error && <Catalog isLoading={isFetching} flowers={availableFlowers} favorites={favoriteFlowersIds} onchangeFavorite={handleFavoriteStatus} />}
+        <Catalog />
       </main>
       <Footer />
-    </>
+    </FavoriteContextProvider>
   );
 }
 
+export default App
+
+// import { useState, useEffect } from "react";
+// import Catalog from "./components/Catalog/Catalog";
+// import Footer from "./components/Footer";
+// import Header from "./components/Header";
+// import Slogan from "./components/Slogan";
+// import { updateFavoriteFlowersIds, fetchAvailableFlowers, fetchFavoriteFlowers } from "./http";
+
+
 // function App() {
 //   const [availableFlowers, setAvailableFlowers] = useState([]);
-//   const [favoriteFlowers, setFavoriteFlowers] = useState([]);
+//   const [favoriteFlowersIds, setFavoriteFlowersIds] = useState([]);
+//   const [isFetching, setIsFetching] = useState(false);
+//   const [error, setError] = useState();
+//   const [errorUpdatingFavorites, setErrorUpdatingFavorites] = useState();
+
 
 //   useEffect(() => {
 //     async function getFlowers() {
-//       const res = await fetch('http://localhost:3000/flowers');
-//       const data = await res.json();
-//       setAvailableFlowers(data);
+//       setIsFetching(true);
+//       try {
+//         const [availableFlowers, favoriteFlowers] = await Promise.all([
+//           fetchAvailableFlowers(),
+//           fetchFavoriteFlowers()
+//         ]);
+
+//         setAvailableFlowers(availableFlowers);
+//         setFavoriteFlowersIds(favoriteFlowers);
+//       } catch (error) {
+//         setError(error.message || 'Ошибка загрузки данных');
+//       }
+//       setIsFetching(false);
 //     }
 //     getFlowers();
 //   }, []);
 
-
-//   useEffect(() => {
-//     async function getFavoriteFlowers() {
-//       const res = await fetch('http://localhost:3000/favorite');
-//       const data = await res.json();
-//       if (!res.ok) {
-//         throw new Error('Ошибка загрузки избранного');
-//       }
-//       setFavoriteFlowers(data);
-//     }
-//     getFavoriteFlowers();
-//   }, []);
-
-//   async function handleAddFavorite(flower) {
-//     setFavoriteFlowers((prevFavoriteFlowers) => {
-//       return [...prevFavoriteFlowers, flower]
+//   async function handleAddFavorite(flowerId) {
+//     setFavoriteFlowersIds((prevFavoriteFlowersIds) => {
+//       return [...prevFavoriteFlowersIds, flowerId]
 //     });
 
 //     try {
-//       await updateFavoriteFlowers([...favoriteFlowers, flower])
+//       await updateFavoriteFlowersIds([...favoriteFlowersIds, flowerId])
 //     } catch (error) {
-//       setFavoriteFlowers(favoriteFlowers);
-//       console.log(error)
+//       setFavoriteFlowersIds(favoriteFlowersIds);
+//       setErrorUpdatingFavorites(error.message || 'Ошибка загрузки данных');
 //     }
 //   }
 
-//   async function handleRemoveFavorite(id) {
-//     setFavoriteFlowers((prevFavoriteFlowers) => {
-//         return prevFavoriteFlowers.filter(item => item.id !== id)
+//   async function handleRemoveFavorite(flowerId) {
+//     setFavoriteFlowersIds((prevFavoriteFlowersIds) => {
+//       return prevFavoriteFlowersIds.filter(id => id !== flowerId)
 //     });
 
 //     try {
-//       await updateFavoriteFlowers(favoriteFlowers.filter(item => item.id !== id));
-//     } catch(error) {
-//       setFavoriteFlowers(favoriteFlowers);
+//       await updateFavoriteFlowersIds(favoriteFlowersIds.filter(id => id !== flowerId));
+//     } catch (error) {
+//       setFavoriteFlowersIds(favoriteFlowersIds);
 //       console.log(error)
-//     }   
-//   }
-
-//   function handleFavoriteStatus(flower, isFavorite) {
-//     console.log(flower, isFavorite)
-//     if (!isFavorite) {
-//       handleAddFavorite(flower);
-//     } else {
-//       handleRemoveFavorite(flower.id);
 //     }
 //   }
+
+//   function handleFavoriteStatus(flowerId, isFavorite) {
+//     if (!isFavorite) {
+//       handleAddFavorite(flowerId);
+//     } else {
+//       handleRemoveFavorite(flowerId);
+//     }
+//   } 
+
 
 //   return (
 //     <>
 //       <Header />
 //       <main>
 //         <Slogan />
-//         <Catalog flowers={availableFlowers} favorites={favoriteFlowers} onchangeFavorite={handleFavoriteStatus} />
+//         {error && <p>Ошибка</p>}
+//         {!error && <Catalog isLoading={isFetching} flowers={availableFlowers} favorites={favoriteFlowersIds} onchangeFavorite={handleFavoriteStatus} />}
 //       </main>
 //       <Footer />
 //     </>
 //   );
 // }
 
-export default App
+// export default App
--- src/components/Catalog/CatalogBlock.jsx
+++ src/components/Catalog/CatalogBlock.jsx
@@ -1,20 +1,53 @@
+import { useState, useEffect, useContext } from "react";
+import { fetchAvailableFlowers, fetchFavoriteFlowers } from "../../http.js";
 import CatalogItem from './CatalogItem';
 import classes from './catalogBlock.module.css';
+import FavoriteContext from '../../store/FavoriteContext';
 
-export default function CatalogBlock({ flowers, favorites, onchangeFavorite }) {
+
+export default function CatalogBlock() {
+  const [availableFlowers, setAvailableFlowers] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState();
+  const { favorites, setFavoriteIds } = useContext(FavoriteContext);
+
+  useEffect(() => {
+    async function getFlowers() {
+      setIsFetching(true);
+      try {
+        const [availableFlowers, favoriteFlowers] = await Promise.all([
+          fetchAvailableFlowers(),
+          fetchFavoriteFlowers()
+        ]);
+
+        setAvailableFlowers(availableFlowers);
+        setFavoriteIds(favoriteFlowers);
+      } catch (error) {
+        setError(error.message || 'Ошибка загрузки данных');
+      }
+      setIsFetching(false);
+    }
+    getFlowers();
+  }, []);
 
   return (
-    <div className={classes.catalogBlock}>
-      <ul className={classes.catalogList}>
-        {flowers.map((flower) => (
-          <CatalogItem
-            key={flower.id}
-            onchangeFavorite={onchangeFavorite}
-            isFavorite={favorites.some((favFlowerId) => flower.id === favFlowerId)}
-            flower={flower}/>
-        ))}
-      </ul>
-    </div>
+    <>
+      {isFetching && <p>ЗАГРУЗКА!!!!!</p>}
+      {error && <p>ОШИБКА</p>}
+      {!isFetching && !error && availableFlowers.length === 0 && 'Похоже нет подходящих вариантов'}
+      {!isFetching && !error && availableFlowers.length > 0 && (
+        <div className={classes.catalogBlock}>
+          <ul className={classes.catalogList}>
+            {availableFlowers.map((flower) => (
+              <CatalogItem
+                key={flower.id}
+                isFavorite={favorites.some((favFlowerId) => flower.id === favFlowerId)}
+                flower={flower} />
+            ))}
+          </ul>
+        </div>
+      )}
+    </>
   );
 }
 // export default function CatalogBlock({ flowers, favorites, onchangeFavorite }) {
