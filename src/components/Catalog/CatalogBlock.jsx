bc7901bbabaf80e252ac9525265c98a6ab8c7f0c
Nataly Tsvetkova <mrhoking666@gmail.com>
2024-02-26 13:01:57 +0300

    fixed path to css stles

--- src/components/Catalog/CatalogBlock.jsx
+++ src/components/Catalog/CatalogBlock.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import CatalogItem from './CatalogItem';
-import classes from './catalogBlock.module.css';
+import classes from './CatalogBlock.module.css';
 import FavoriteContext from '../../store/FavoriteContext';
 
 export default function CatalogBlock({isFetching, error, availableFlowers}) {
