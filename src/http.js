3ffcd1095a08b891bed8b094dba5c2800bbe04a6
Nataly Tsvetkova <mrhoking666@gmail.com>
2024-02-26 18:47:42 +0300

    added fetch for cart

--- src/http.js
+++ src/http.js
@@ -42,3 +42,30 @@ export async function createOrder(order) {
 
   return data;
 }
+
+export async function fetchCart(){
+  const response = await fetch('https://flower-app-b21ff-default-rtdb.europe-west1.firebasedatabase.app/cart.json');
+  const data = await response.json();
+  if (!response.ok) {
+    throw new Error('Ошибка загрузки корзины');
+  }
+  return data;
+}
+
+export async function updateCart(cart) {
+  const response = await fetch('https://flower-app-b21ff-default-rtdb.europe-west1.firebasedatabase.app/cart.json', {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(cart)
+  })
+
+  const data = response.json();
+
+  if (!response.ok) {
+    throw new Error('Ошибка при обновлении состояния корзины');
+  }
+
+  return data;
+}
